a45909fa5a0af18a322ec42d55861f97b48d4819
fredrik-p <fredrik.pekkarinen@gmail.com>
2024-02-03 19:09:20 +0100

    thinner font in some places

--- src/app/utstallningar/page.tsx
+++ src/app/utstallningar/page.tsx
@@ -7,10 +7,10 @@ const Exhibitions = () => {
             <main className="flex flex-col items-center justify-center md:h-[80vh] max-w-md mx-auto">
                 <div className="p-6">
                     <h2 className="text-lg mb-3">Utställningar</h2>
-                    <p className="mb-3">Capellagårdens sommarutställning, 2021–2023</p>
-                    <p className="mb-3">Vida konsthall, 2022</p>
-                    <p className="mb-3">Sydosten, Kalmar konstmuseum, 2022</p>
-                    <p className="mb-3">Trä Art, Kulturgatan Bodafors, 2022</p>
+                    <p className="mb-3 font-extralight">Capellagårdens sommarutställning, 2021–2023</p>
+                    <p className="mb-3 font-extralight">Vida konsthall, 2022</p>
+                    <p className="mb-3 font-extralight">Sydosten, Kalmar konstmuseum, 2022</p>
+                    <p className="mb-3 font-extralight">Trä Art, Kulturgatan Bodafors, 2022</p>
                 </div>
             </main>
         </>
