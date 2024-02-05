033551c5b559896f966d28f19cd9c97dfdd46755
fredrik-p <fredrik.pekkarinen@gmail.com>
2024-02-05 22:57:36 +0100

    added sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,42 @@
+import { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://studiohagel.com',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://studiohagel.com/om',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+        url: 'https://studiohagel.com/portfolio',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.5,
+      },
+      {
+        url: 'https://studiohagel.com/utstallningar',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.5,
+      },
+      {
+        url: 'https://studiohagel.com/kurser',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.5,
+      },
+      {
+        url: 'https://studiohagel.com/kontakt',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.5,
+      },
+  ]
+}
