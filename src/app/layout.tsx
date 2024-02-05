a5fc70df9814411d2ec63701dcef7d7190cc0372
fredrik-p <fredrik.pekkarinen@gmail.com>
2024-02-05 22:33:53 +0100

    fix og img

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const outfit = Outfit({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: 'hagel studio',
   description: 'Möbelsnickeri och formgivning',
-  metadataBase: new URL('https://hagelstudio-nqf91z4d6-fredrik-ps-projects.vercel.app/opengraph-image.png?19b18e3828374e52'),
+  metadataBase: new URL('https://hagelstudio-nqf91z4d6-fredrik-ps-projects.vercel.app/'),
   openGraph: {
     title: 'hagel studio',
     description: 'Möbelsnickeri och formgivning',
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
     images:
     [
       {
-        url: 'https//hagelstudio.com/opengraph-image.png',
+        url: 'https://hagelstudio.com/opengraph-image.png',
       }, 
     ],
     locale: 'sv_SE',
