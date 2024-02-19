b5d38b642778033386e01fd5f163766d3e170ad8
fredrik-p <fredrik.pekkarinen@gmail.com>
2024-02-20 00:45:53 +0100

    faulty url in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,25 +3,25 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://studiohagel.com',
+      url: 'https://hagelstudio.com',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://studiohagel.com/om',
+      url: 'https://hagelstudio.com/om',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-        url: 'https://studiohagel.com/portfolio',
+        url: 'https://hagelstudio.com/portfolio',
         lastModified: new Date(),
         changeFrequency: 'monthly',
         priority: 0.7,
       },
       {
-        url: 'https://studiohagel.com/utstallningar',
+        url: 'https://hagelstudio.com/utstallningar',
         lastModified: new Date(),
         changeFrequency: 'monthly',
         priority: 0.6,
